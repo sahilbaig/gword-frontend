a0b439c3989f5a3e9bbc993a97005176c51a7c73
Sahil Baig <sahilbaig.me@gmail.com>
2025-03-06 18:07:26 +0530

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
     <div className="">
       {user ? (
         <div className="p-6 bg-green-500 text-white text-xl rounded-lg">
-          <p>Welcome, {user.displayName}!</p>
           <button
             className="mt-4 bg-red-500 px-4 py-2 rounded"
             onClick={logout}
